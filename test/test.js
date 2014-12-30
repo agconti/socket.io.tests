a989a8921b344bce9a8675a7ed9c0adae69cfad0
agconti <agc11d@gmail.com>
2014-12-30 12:28:55 -0500

    feat(gitignore): updated to include .DS_Store

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var expect = require('chai').expect
+  , server = require('./utils/server')
   , io = require('socket.io/node_modules/socket.io-client')
   , ioOptions = { 
       transports: ['websocket']
@@ -17,8 +18,7 @@ describe('Chat Events', function(){
   beforeEach(function(done){
     
     // start the io server
-    var server = require('../index.js').server
-
+    server.start()
     // connect two io clients
     sender = io('http://localhost:3000/', ioOptions)
     receiver = io('http://localhost:3000/', ioOptions)
